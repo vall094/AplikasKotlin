d6ab971ddaf756d288d212af8c865cd141113272
Novalf <novalfajarbudipamungkas@gmail.com>
2024-02-22 14:29:01 +0700

    tugas fungsi tentang biodata

--- app/src/main/java/com/Ti2A/aplikasikotlin/Hello.kt
+++ app/src/main/java/com/Ti2A/aplikasikotlin/Hello.kt
@@ -1,9 +1,15 @@
 package com.Ti2A.aplikasikotlin
 
 
-        fun main(args: Array<String>) {
-            println("hello, ${args[0]}")
-            val temperature = 20
-                    val isHot = if (temperature > 40) "Panas" else "dingin"
-            println(isHot)
-        }
+class Person(val name: String, val hobby: Hobby)
+
+class Hobby(val name: String, val description: String)
+
+fun main() {
+    val hobby = Hobby("Main Game", "Saya suka memaikan game, terutama memainkan game mobile legends karena saya sangat jago")
+    val person = Person("Noval Fajar Budi Pamungkas", hobby)
+
+    println("Nama: ${person.name}")
+    println("Hobi: ${person.hobby.name}")
+    println("Deskripsi Hobi: ${person.hobby.description}")
+}
