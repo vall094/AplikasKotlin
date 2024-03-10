4243d1999c5ebe76012a18372006817a2e34d106
Novalf <novalfajarbudipamungkas@gmail.com>
2024-03-10 21:21:03 +0700

    tugas

--- app/src/main/java/com/Ti2A/aplikasikotlin/example/myapp/Aquarium.kt
+++ app/src/main/java/com/Ti2A/aplikasikotlin/example/myapp/Aquarium.kt
@@ -1,4 +1,35 @@
 package com.Ti2A.aplikasikotlin.example.myapp
 
-class Aquarium {
+class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+        // Dimensions in cm
+        var length: Int = length
+        var width: Int = width
+        var height: Int = height
+
+        fun printSize() {
+            println(
+                "Width: $width cm " +
+                        "Length: $length cm " +
+                        "Height: $height cm "
+            )
+            // 1 liter = 1000 cm^3
+            println("Volume: $volume liters")
+        }
+
+        init {
+            println("aquarium initializing")
+        }
+
+        constructor(numberOfFish: Int) : this() {
+            // 2,000 cm^3 per fish + extra room so water doesn't spill
+            val tank = numberOfFish * 2000 * 1.1
+            // calculate the height needed
+            height = (tank / (length * width)).toInt()
+        }
+
+        var volume: Int
+            get() = width * height * length / 1000
+            set(value) {
+                height = (value * 1000) / (width * length)
+            }
 }
--- app/src/main/java/com/Ti2A/aplikasikotlin/example/myapp/main.kt
+++ app/src/main/java/com/Ti2A/aplikasikotlin/example/myapp/main.kt
@@ -1,2 +1,12 @@
 package com.Ti2A.aplikasikotlin.example.myapp
 
+fun buildAquarium() {
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
+}
+
+fun main() {
+    buildAquarium()
+}
