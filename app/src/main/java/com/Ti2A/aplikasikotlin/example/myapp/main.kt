ee2c67d4922d7499bf4728af538a2da4786cded8
Novalf <novalfajarbudipamungkas@gmail.com>
2024-03-18 10:44:21 +0700

    Up tugas

--- app/src/main/java/com/Ti2A/aplikasikotlin/example/myapp/main.kt
+++ app/src/main/java/com/Ti2A/aplikasikotlin/example/myapp/main.kt
@@ -1,28 +1,8 @@
 package com.Ti2A.aplikasikotlin.example.myapp
 
-import java.lang.Math.PI
-
-fun buildAquarium() {
-    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
-    myAquarium.printSize()
-    val myTower = TowerTank(diameter = 25, height = 40)
-    myTower.printSize()
-}
-
-fun makeFish() {
-    val shark = Shark()
-    val pleco = Plecostomus()
-    println("Shark: ${shark.color}")
-    shark.eat()
-    println("Plecostomus: ${pleco.color}")
-    pleco.eat()
-}
-
-fun main (){
-    buildAquarium()
-    makeFish()
-}package com.example.aplikasikotlin.example.myapp
-
+import com.Ti2A.aplikasikotlin.example.myapp.Decor.Plecostomus
+import com.Ti2A.aplikasikotlin.example.myapp.Decor.Shark
+import com.Ti2A.aplikasikotlin.example.myapp.Decor.TowerTank
 import java.lang.Math.PI
 
 fun buildAquarium() {
